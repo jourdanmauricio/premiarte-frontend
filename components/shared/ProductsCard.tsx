3a5a230d65ac00b421e1ce3b63666fb7d29d0258
mauricio <jourdanmauricio@gmail.com>
2026-02-24 17:18:10 -0300

    .

--- components/shared/ProductsCard.tsx
+++ components/shared/ProductsCard.tsx
@@ -1,9 +1,10 @@
 import { Product } from "@/app/shared/types";
+import { Link } from "next-view-transitions";
 import Image from "next/image";
 
 const ProductsCard = ({ product }: { product: Product }) => {
   return (
-    <a
+    <Link
       href={`/productos/${product.slug}`}
       className="shadow-xl transition-all duration-300 hover:drop-shadow-3xl hover:shadow-orange-200/10"
     >
@@ -14,6 +15,7 @@ const ProductsCard = ({ product }: { product: Product }) => {
           width={256}
           height={256}
           className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105 img-appear"
+          style={{ viewTransitionName: `product-image-${product.slug}` }}
         />
       </div>
 
@@ -21,17 +23,13 @@ const ProductsCard = ({ product }: { product: Product }) => {
         <div className="pill mx-auto">{product.categories[0].name}</div>
         <h3
           className="font-medium"
-          style={
-            {
-              // viewTransitionName: transitionNames.title
-            }
-          }
+          style={{ viewTransitionName: `product-title-${product.slug}` }}
         >
           {product.name}
         </h3>
         <div className="flex justify-center gap-2"></div>
       </div>
-    </a>
+    </Link>
   );
 };
 
